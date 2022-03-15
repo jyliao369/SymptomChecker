38c2aab9162d767f8a5638e5134b8fb74c22edfd
jyliao369 <jyliao369@gmail.com>
2022-03-15 15:47:45 -0400

    added a .env to hide keys

--- src/components/SymptomChecker.vue
+++ src/components/SymptomChecker.vue
@@ -198,9 +198,8 @@ export default {
       def: "",
       def2: "",
       defList: [],
-      APItoken:
-        "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6InN0YXJyeWtuaWdodDM2OUBnbWFpbC5jb20iLCJyb2xlIjoiVXNlciIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL3NpZCI6IjEwNDAxIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy92ZXJzaW9uIjoiMjAwIiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9saW1pdCI6Ijk5OTk5OTk5OSIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbWVtYmVyc2hpcCI6IlByZW1pdW0iLCJodHRwOi8vZXhhbXBsZS5vcmcvY2xhaW1zL2xhbmd1YWdlIjoiZW4tZ2IiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL2V4cGlyYXRpb24iOiIyMDk5LTEyLTMxIiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9tZW1iZXJzaGlwc3RhcnQiOiIyMDIyLTAzLTA2IiwiaXNzIjoiaHR0cHM6Ly9zYW5kYm94LWF1dGhzZXJ2aWNlLnByaWFpZC5jaCIsImF1ZCI6Imh0dHBzOi8vaGVhbHRoc2VydmljZS5wcmlhaWQuY2giLCJleHAiOjE2NDczNzMyODQsIm5iZiI6MTY0NzM2NjA4NH0._uZsvV8B4KysGKt-g7GtuBq7tRd7XVYqfYyCl8m3_p8",
-      termToken: "20ede4fa-246b-439a-a38d-d9f84101fd1b",
+      APItoken: process.env.VUE_APP_MEDICAPI_KEY,
+      termToken: process.env.VUE_APP_DICTIONARY_KEY,
     };
   },
   components: {
